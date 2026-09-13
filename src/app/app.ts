0e131aaeab6c2e0ae7595f78020c67a48b8ac908
raybellido <raaygb@gmail.com>
2026-09-13 12:54:20 -0500

    quitando prueva de cd

--- src/app/app.ts
+++ src/app/app.ts
@@ -10,7 +10,6 @@ import { Toast } from './components/toast/toast';
   imports: [RouterOutlet, Navbar, Toast, ConfirmDialog],
   template: `
     <app-navbar />
-    <h1 >hola mundoooo</h1>
     <main class="min-h-[calc(100dvh-4rem)]">
       <router-outlet />
     </main>
